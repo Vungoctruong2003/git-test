5f2fbe23e02f1569e86e05447c25071129392851
Ngọc Trưởng <truongvugh2003@gmail.com>
2021-12-11 10:33:30 +0700

    t

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TruongComponent } from './truong/truong.component';
-import { Truong3Component } from './truong3/truong3.component';
+import { DuongComponent } from './duong/duong.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TruongComponent,
-    Truong3Component
+    DuongComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/duong/duong.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-duong',
+  templateUrl: './duong.component.html',
+  styleUrls: ['./duong.component.css']
+})
+export class DuongComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
