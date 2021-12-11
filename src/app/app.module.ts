2fbc868571e6edcd9e8d3b97a69807d73d069b57
jimhan1992 <tranhan1992@gmail.com>
2021-12-11 10:26:49 +0700

    han1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< HEAD
 import { HanComponent } from './han/han.component';
+=======
+import { TruongComponent } from './truong/truong.component';
+>>>>>>> 077e3786fd759f4c0ca65eea2dda78ab6c278bbe
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< HEAD
     HanComponent
+=======
+    TruongComponent
+>>>>>>> 077e3786fd759f4c0ca65eea2dda78ab6c278bbe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/truong/truong.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TruongComponent } from './truong.component';
+
+describe('TruongComponent', () => {
+  let component: TruongComponent;
+  let fixture: ComponentFixture<TruongComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TruongComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TruongComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
